fff0e9e2d85be5be360e6636200cc1699c58154d
Martin Volek <martin@vdolek.cz>
2015-10-14 15:38:57 +0200

    Models

---
+++ src/KnapsackProblem/Model/Instance.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+
+namespace Cz.Volek.CVUT.FIT.MIPAA.KnapsackProblem.Model
+{
+    public class Instance
+    {
+        public int Id { get; set; }
+
+        public int Capacity { get; set; }
+
+        public IList<Item> Items { get; set; }
+
+        public int ItemCount => Items.Count;
+    }
+}
---
+++ src/KnapsackProblem/Model/Item.cs
@@ -0,0 +1,9 @@
+namespace Cz.Volek.CVUT.FIT.MIPAA.KnapsackProblem.Model
+{
+    public class Item
+    {
+        public int Price { get; set; }
+
+        public int Wights { get; set; }
+    }
+}
